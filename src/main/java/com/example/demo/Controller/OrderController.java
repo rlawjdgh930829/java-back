79d1fdfa327d567e973eda7cd9b5ea9b8dab2a78
Kim Jeongho <kimjeongho930829@gmail.com>
2022-11-07 16:56:26 +0900

    주문 및 주문내역

---
+++ src/main/java/com/example/demo/Controller/OrderController.java
@@ -0,0 +1,59 @@
+package com.example.demo.Controller;
+
+import com.example.demo.dto.OrderDto;
+import com.example.demo.entity.Order;
+import com.example.demo.repository.OrderRepository;
+import com.example.demo.service.JwtService;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    JwtService jwtService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @GetMapping("/api/orders")
+    public ResponseEntity getOrder(
+            @CookieValue(value = "token", required = false) String token
+    ) {
+        if(!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        List<Order> orders = orderRepository.findAll();
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @PostMapping("/api/orders")
+    public ResponseEntity pushOrder(
+            @RequestBody OrderDto dto,
+            @CookieValue(value = "token", required = false) String token
+    ) {
+        if(!jwtService.isValid(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        Order newOrder = new Order();
+        newOrder.setMemberId(jwtService.getId(token));
+        newOrder.setName(dto.getName());
+        newOrder.setAddress(dto.getAddress());
+        newOrder.setPayment(dto.getPayment());
+        newOrder.setCardNumber(dto.getCardNumber());
+        newOrder.setItems(dto.getItems());
+
+        orderRepository.save(newOrder);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
